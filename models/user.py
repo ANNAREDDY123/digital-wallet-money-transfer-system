6c77d2b9664bfa296a293121a71e141ef74598e8
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-06-18 10:23:11 +0530

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    name = Column(String)
+
+    email = Column(String, unique=True)
+
+    phone = Column(String)
+
+    password = Column(String)
+
+    role = Column(String)
