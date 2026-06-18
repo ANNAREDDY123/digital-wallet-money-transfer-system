8d4b8717445e5959175126076c1b5148b29a7439
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-06-18 16:09:43 +0530

    Update transfer.py

--- schemas/transfer.py
+++ schemas/transfer.py
@@ -1,13 +1,7 @@
-from pydantic import (
-    BaseModel,
-    Field
-)
+from pydantic import BaseModel, Field
 
 
 class TransferMoney(BaseModel):
-
     sender_wallet_id: int
-
     receiver_wallet_id: int
-
     amount: float = Field(gt=0)
